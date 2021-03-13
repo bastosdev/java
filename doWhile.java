ccbcb98c22c15e2d22f260117ccef55e45a1e6ce
Bruno Bastos <bastosdev@outlook.com>
2021-03-13 16:15:43 -0300

    Create doWhile.java

---
+++ doWhile.java
@@ -0,0 +1,15 @@
+public class doWhile {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        int max = 10;
+
+        do {
+            count++;
+            System.out.println("O valor é " +count);
+        } while (count <= max);
+
+    }
+
+}
