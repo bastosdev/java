2e074d7e508563b94882dbe27d711d672e7eb23c
Bruno Bastos <bastosdev@outlook.com>
2021-03-13 12:39:44 -0300

    Create SwitchCase.java

---
+++ SwitchCase.java
@@ -0,0 +1,29 @@
+public class SwitchCase {
+
+    public static void main(String[] args) {
+
+        int diaUtil = 1;
+
+        switch (diaUtil){
+
+            case 1:
+                System.out.println("Segunda");
+                break;
+            case 2:
+                System.out.println("Terça");
+                break;
+            case 3:
+                System.out.println("Quarta");
+                break;
+            case 4:
+                System.out.println("Quinta");
+                break;
+            case 5:
+                System.out.println("Sexta");
+                break;
+            default:
+                System.out.println("Não é um dia Útil");
+        }
+
+    }
+}
