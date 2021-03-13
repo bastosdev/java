a1716bbc4e407f055ef399fdac98ad9b51f1e327
Bruno Bastos <bastosdev@outlook.com>
2021-03-13 16:09:45 -0300

    Create While.java

---
+++ While.java
@@ -0,0 +1,16 @@
+public class While {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        int max = 10;
+
+        System.out.println("Contando até " + max);
+
+        while (count <= max) {
+            System.out.println("O valor é: " + count);
+            count++;
+        }
+
+    }
+}
