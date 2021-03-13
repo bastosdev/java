e163bebdf928409bc68a973dfda93bc685d59f75
Bruno Bastos <bastosdev@outlook.com>
2021-03-13 20:44:58 -0300

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        double[] numbers = new double[4];
+        numbers[0] = 31.3;
+        numbers[1] = 32.5;
+        numbers[2] = 33.7;
+        numbers[3] = 34.2;
+
+        for (int print = 0; print < numbers.length; print++){
+            System.out.println(numbers[print]);
+        }
+
+
+    }
+
+}
