8ae110cb016cc322f17cd162ab55824c0c59ea7d
Bruno Bastos <bastosdev@outlook.com>
2021-03-13 12:16:49 -0300

    Create ifElse.java

---
+++ ifElse.java
@@ -0,0 +1,16 @@
+public class ifElse {
+
+    public static void main(String[] args) {
+
+        int nota = 7;
+
+        if (nota >= 10){
+            System.out.println("Aprovado!");
+        } else if (nota <= 7){
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+
+    }
+}
