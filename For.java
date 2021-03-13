7afa90dbacffc20cef838619555fec79f3d2db27
Bruno Bastos <bastosdev@outlook.com>
2021-03-13 16:31:10 -0300

    Create For.java

---
+++ For.java
@@ -0,0 +1,13 @@
+public class For {
+
+    public static void main(String[] args) {
+
+        int max = 10;
+
+        for (int count = 0; count <= max; count++){
+            System.out.println("O valor é " + count);
+        }
+
+    }
+
+}
