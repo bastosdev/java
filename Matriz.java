848d0fe366246dac166c24d6df6926191e069c2f
Bruno Bastos <bastosdev@outlook.com>
2021-03-14 09:15:59 -0300

    Create Matriz.java

---
+++ Matriz.java
@@ -0,0 +1,20 @@
+public class Matriz {
+
+    public static void main(String[] args) {
+
+        double[][] numbers = new double [2][2];
+        numbers[0][0] = 0.0;
+        numbers[0][1] = 0.1;
+
+        numbers[1][0] = 1.0;
+        numbers[1][1] = 1.1;
+
+        for (int i = 0; i < numbers.length; i++){
+            for (int j = 0; j < numbers[i].length; j++){
+                System.out.println(numbers[i][j]);
+            }
+        }
+
+    }
+
+}
