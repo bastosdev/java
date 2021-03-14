83c9b6416c8066ea75f1f72b5b3df9641b69eef2
Bruno Bastos <bastosdev@outlook.com>
2021-03-14 10:28:07 -0300

    Create Foreach.java

---
+++ Foreach.java
@@ -0,0 +1,15 @@
+public class Foreach {
+
+    public static void main(String[] args) {
+
+        String[] names = new String[2];
+        names[0] = "Bruno";
+        names[1] = "Suelen";
+
+        for (String people : names) {
+            System.out.println(people);
+        }
+
+    }
+
+}
